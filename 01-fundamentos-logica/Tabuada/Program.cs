8222e6cb95dbe15429ff959aa05724a7a1ac4f87
BrazD <brazdanarantes@gmail.com>
2026-01-08 16:07:04 -0300

    feat: adiciona projeto de tabuada simples com validação de entrada

---
+++ 01-fundamentos-logica/Tabuada/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+Console.WriteLine("===Tabuada Simples===");
+
+Console.Write("Digite um número inteiro: ");
+ bool numeroValido = int.TryParse(Console.ReadLine(), out int numero);
+
+for(int i=0; i<=10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero*i}");
+}
