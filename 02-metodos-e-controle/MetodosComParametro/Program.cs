561de2bdf9078ad6bae62ac48a4742a294b8a2f6
BrazD <brazdanarantes@gmail.com>
2026-01-08 16:30:49 -0300

    feat: método com parâmetro para leitura de número

---
+++ 02-metodos-e-controle/MetodosComParametro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+static double LerNumero(string mensagem)
+{
+    Console.Write(mensagem);
+    bool numeroValido = double.TryParse(Console.ReadLine(), out double numero);
+    return numero;
+}
+Console.WriteLine("+++Soma de dois números+++");
+double a = LerNumero("Digite o primeiro número: ");
+double b = LerNumero("Digite o segundo número: ");
+
+Console.WriteLine($"a Soma de {a} com {b} é : {a + b}");
