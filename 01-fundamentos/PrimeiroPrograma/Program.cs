308dda48896e2a4d965d39cae6662020c701e2cd
BrazD <brazdanarantes@gmail.com>
2026-01-08 09:38:32 -0300

    feat: primeiro programa com entrada de dados e variáveis

---
+++ 01-fundamentos/PrimeiroPrograma/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.WriteLine("===Cadastro Simples====");
+
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Olá {nome}, você tem {idade} anos");
