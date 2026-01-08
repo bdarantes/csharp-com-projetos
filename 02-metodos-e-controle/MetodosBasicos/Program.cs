596d2c511aa6960b203817fca1de812df68295fa
BrazD <brazdanarantes@gmail.com>
2026-01-08 16:20:24 -0300

    feat: cria primeiro método para leitura de número

---
+++ 02-metodos-e-controle/MetodosBasicos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+static double lerNumero()
+{
+    Console.Write("Digite um número: ");
+    bool numeroValido = double.TryParse(Console.ReadLine(), out double numero);
+    return numero;
+}
+
+Console.WriteLine("Você irá digitar 2 números para serem somados");
+double a = lerNumero();
+double b = lerNumero();
+
+Console.WriteLine($"A soma entre {a} e {b} é: {a + b}");
