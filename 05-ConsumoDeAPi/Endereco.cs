28031431b5599a53bd33aad16dd5efb38d634724
BrazD <brazdanarantes@gmail.com>
2026-02-08 12:00:28 -0300

    feat: cria projeto IntegracaoViaCep em 05-ConsumoDeApi com model Endereco

---
+++ 05-ConsumoDeAPi/Endereco.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+public class Endereco
+{
+    [JsonPropertyName("cep")]
+    public string Cep { get; set; }
+
+    [JsonPropertyName("logradouro")]
+    public string Logradouro { get; set; }
+
+    [JsonPropertyName("complemento")]
+    public string complemento { get; set; }
+
+    [JsonPropertyName("bairro")]
+    public string Bairro { get; set; }
+
+    [JsonPropertyName("localidade")]
+    public string Cidade { get; set; }
+
+    [JsonPropertyName("uf")]
+    public string Uf { get; set; }
+
+    [JsonPropertyName("erro")]
+    public string Erro {get; set; }
+}
