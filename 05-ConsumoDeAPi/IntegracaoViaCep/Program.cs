27eab512f5962b9f5fcdd3dd0f7b3c5279168a2b
BrazD <brazdanarantes@gmail.com>
2026-02-08 12:31:11 -0300

    feat: implementa a lógica e a interação com o usuário no Main do projeto 05-ConsumoDeApi/IntegracaoViaCep

--- 05-ConsumoDeAPi/IntegracaoViaCep/Program.cs
+++ 05-ConsumoDeAPi/IntegracaoViaCep/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("===Integração Via Cep - Iniciando===");
+
+ViaCepService service = new ViaCepService();
+
+while (true)
+{
+    Console.Write("\nDigite o CEP (ou 'sair): ");
+    string cepInput = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(cepInput) || cepInput.ToLower() == "sair")
+    break;
+
+Console.WriteLine("Buscando....");
+
+var endereco = await service.BuscarEndereco(cepInput);
+
+if (endereco == null || endereco.Erro == "true")
+    {
+        Console.WriteLine("CEP não encontrado!");
+    }
+else
+    {
+        Console.WriteLine("Endereço Localizado:");
+        Console.WriteLine($"-> {endereco.Logradouro}");
+        Console.WriteLine($"-> {endereco.Bairro}");
+        Console.WriteLine($"-> {endereco.Cidade} - {endereco.Uf}");
+        
+    }
+}
+
+Console.WriteLine("=== Encerrando Integração Via Cep ===");
